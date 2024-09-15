138624d5f4dca2e9b304cb606ee3b775a1693ccd
Arslan Ali <muhammadarslanrajput12@gmail.com>
2024-09-15 09:19:11 +0500

    Nested Array Example

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+let matrix1 = [
+    [1, 12, 55],
+    [14, 1, 67],
+    [78, 2, 23]
+];
+let matrix2 = [
+    [1, 7, 5],
+    [14, 1, 6],
+    [8, 3, 13]
+];
+const result = [[]];
+
+for (let i = 0; i < matrix1.length; i++) {
+    for (let j = 0; j < matrix2.length; j++) {
+        const sum = matrix1[i][j] + matrix2[i][j]
+        result[0].push(sum)
+
+    }
+
+}
+console.log(result)
